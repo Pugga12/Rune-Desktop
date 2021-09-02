204a7c7df0f1e33550f4ef8a2ac633913fabd23b
Pugga12 <64217755+Pugga12@users.noreply.github.com>
2021-09-02 17:04:47 -0400

    [Merge Upstream] Merge HTML Changes
    Commit: https://github.com/Pugga12/rune/commit/0f37202
    Also prevents the window from being opened until the install is done

--- main.js
+++ main.js
@@ -3,19 +3,19 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
-
+if (require('electron-squirrel-startup')) return app.quit();
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      //preload: path.join(__dirname, 'preload.js')
     }
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('assets/index.html')
+  mainWindow.loadFile('index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
